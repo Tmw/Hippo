de11d79372eb3e3bb592f1793c7f5505da27298a
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-02-06 15:58:31 +0100

    mark unused variable

--- hippo-backend/lib/hippo_web/resolvers/lane_resolver.ex
+++ hippo-backend/lib/hippo_web/resolvers/lane_resolver.ex
@@ -12,7 +12,7 @@ defmodule HippoWeb.Resolvers.Lane do
   def create(%{lane: params, project_id: project_id}, _) do
     case Lanes.create_lane(params, for_project: project_id) do
       {:ok, _} = result -> result
-      {:error, changeset} -> {:error, "Something blew up"}
+      {:error, _changeset} -> {:error, "Something blew up"}
     end
   end
 
