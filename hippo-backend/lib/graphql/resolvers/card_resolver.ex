f6c31ffeb6d3c9d7a99e4d63c80f95f18a804165
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-02-26 00:29:17 +0100

    Drop unused code for now

--- hippo-backend/lib/graphql/resolvers/card_resolver.ex
+++ hippo-backend/lib/graphql/resolvers/card_resolver.ex
@@ -1,14 +1,6 @@
 defmodule Hippo.GraphQL.Resolvers.Card do
   alias Hippo.Cards
 
-  # def data() do
-  #   Dataloader.Ecto.new(Hippo.Repo, query: &query/2)
-  # end
-
-  # def query(queryable, _params) do
-  #   queryable
-  # end
-
   def create(%{card: params, lane_id: lane_id}, _) do
     case Cards.create_card(params, for_lane: lane_id) do
       {:ok, _} = result -> result
--- hippo-backend/lib/graphql/resolvers/lane_resolver.ex
+++ hippo-backend/lib/graphql/resolvers/lane_resolver.ex
@@ -1,14 +1,6 @@
 defmodule Hippo.GraphQL.Resolvers.Lane do
   alias Hippo.Lanes
 
-  # def data() do
-  #   Dataloader.Ecto.new(Hippo.Repo, query: &query/2)
-  # end
-
-  # def query(queryable, _params) do
-  #   queryable
-  # end
-
   def create(%{lane: params, project_id: project_id}, _) do
     case Lanes.create_lane(params, for_project: project_id) do
       {:ok, _} = result -> result
