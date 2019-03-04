50721f90f0c72c76fceb3c798036229c5aaaa261
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-03-04 13:17:26 +0100

    Spec update and delete mutations for lane

--- hippo-backend/test/integration/lanes_mutations_test.exs
+++ hippo-backend/test/integration/lanes_mutations_test.exs
@@ -7,13 +7,13 @@ defmodule Hippo.Grapql.LanesMutationsTest do
 
   setup %{conn: conn} do
     project = insert(:project)
-    lanes = insert_list(2, :lane, project: project)
+    lane = insert(:lane, project: project)
 
     {:ok,
      %{
        conn: conn,
        project: project,
-       lanes: lanes
+       lane: lane
      }}
   end
 
@@ -79,6 +79,112 @@ defmodule Hippo.Grapql.LanesMutationsTest do
         |> Enum.at(0)
 
       assert error["message"] =~ "project not found"
+    end
+  end
+
+  describe "update_lane_mutation" do
+    @query """
+      mutation UpdateLane($laneId: UUID!, $lane: LaneUpdateParams!){
+        updateLane(laneId: $laneId, lane: $lane) {
+          id
+          title
+          description
+        }
+      }
+    """
+
+    test "updates the lane", %{conn: conn, lane: lane} do
+      lane_params = params_for(:lane)
+
+      variables = %{
+        "laneId" => lane.id,
+        "lane" => lane_params
+      }
+
+      conn = conn |> gql(skeleton(@query, variables))
+
+      response =
+        json_response(conn, 200)
+        |> Map.get("data")
+        |> Map.get("updateLane")
+
+      expected =
+        Lane
+        |> Repo.get(response["id"])
+        |> Map.take(~w(title description)a)
+
+      assert expected == lane_params
+    end
+
+    test "updates when only one params is present", %{conn: conn, lane: lane} do
+      lane_params = %{"description" => "a description"}
+
+      variables = %{
+        "laneId" => lane.id,
+        "lane" => lane_params
+      }
+
+      conn = conn |> gql(skeleton(@query, variables))
+
+      response =
+        json_response(conn, 200)
+        |> Map.get("data")
+        |> Map.get("updateLane")
+
+      lane = Repo.get(Lane, response["id"])
+      assert lane.description == lane_params["description"]
+    end
+
+    test "errors when lane_id is invalid", %{conn: conn} do
+      variables = %{
+        "laneId" => Ecto.UUID.generate(),
+        "lane" => params_for(:lane)
+      }
+
+      conn = conn |> gql(skeleton(@query, variables))
+
+      error =
+        json_response(conn, 200)
+        |> Map.get("errors")
+        |> Enum.at(0)
+
+      assert error["message"] =~ "lane not found"
+    end
+  end
+
+  describe "delete_lane_mutation" do
+    @query """
+      mutation DeleteLane($laneId: UUID!) {
+        deleteLane(laneId: $laneId) {
+          success
+          message
+        }
+      }
+    """
+
+    test "deletes the lane", %{conn: conn, lane: lane} do
+      variables = %{"laneId" => lane.id}
+      conn = conn |> gql(skeleton(@query, variables))
+
+      response =
+        json_response(conn, 200)
+        |> Map.get("data")
+        |> Map.get("deleteLane")
+
+      assert response["success"] == true
+      assert Repo.get(Lane, lane.id) == nil
+    end
+
+    test "errors when lane_id is invalid", %{conn: conn} do
+      variables = %{"laneId" => Ecto.UUID.generate()}
+      conn = conn |> gql(skeleton(@query, variables))
+
+      error =
+        json_response(conn, 200)
+        |> Map.get("errors")
+        |> Enum.at(0)
+
+      assert error["message"] =~ "lane not found"
     end
   end
 end
