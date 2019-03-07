7bb1bbfdd41fb3aca284e163ea0fcca1a392ef05
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-03-08 00:43:57 +0100

    Add :rank integer to lane and card models

--- hippo-backend/priv/repo/migrations/20190116094514_create_lanes.exs
+++ hippo-backend/priv/repo/migrations/20190116094514_create_lanes.exs
@@ -4,6 +4,7 @@ defmodule Hippo.Repo.Migrations.CreateLanes do
   def change do
     create table(:lanes, primary_key: false) do
       add :id, :binary_id, null: false, primary_key: true
+      add :rank, :integer
       add :title, :string
       add :description, :string, size: 500
       add :project_id, references(:projects, on_delete: :nothing, type: :binary_id)
