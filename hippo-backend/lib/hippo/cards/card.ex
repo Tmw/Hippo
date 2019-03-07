9f1ebd7904dcc885eeaf061ab11147fefec99b0f
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-03-08 00:44:45 +0100

    Implement EctoRanked in Card and Lane models

--- hippo-backend/lib/hippo/cards/card.ex
+++ hippo-backend/lib/hippo/cards/card.ex
@@ -1,13 +1,15 @@
 defmodule Hippo.Cards.Card do
   use Ecto.Schema
   import Ecto.Changeset
+  import EctoRanked
 
   @primary_key {:id, Ecto.ULID, autogenerate: true}
   @foreign_key_type Ecto.ULID
   schema "cards" do
     field :title, :string
     field :description, :string
-
+    field :rank, :integer
+    field :position, :any, virtual: true
     belongs_to :lane, Hippo.Lanes.Lane
 
     timestamps()
@@ -16,7 +18,8 @@ defmodule Hippo.Cards.Card do
   @doc false
   def changeset(card, attrs) do
     card
-    |> cast(attrs, [:title, :description])
+    |> cast(attrs, [:title, :description, :position])
     |> validate_required([:title])
+    |> set_rank(scope: :lane_id)
   end
 end
--- hippo-backend/lib/hippo/lanes/lane.ex
+++ hippo-backend/lib/hippo/lanes/lane.ex
@@ -1,13 +1,15 @@
 defmodule Hippo.Lanes.Lane do
   use Ecto.Schema
   import Ecto.Changeset
+  import EctoRanked
 
   @primary_key {:id, Ecto.ULID, autogenerate: true}
   @foreign_key_type Ecto.ULID
   schema "lanes" do
     field :title, :string
     field :description, :string
-
+    field :rank, :integer
+    field :position, :any, virtual: true
     belongs_to :project, Hippo.Projects.Project
     has_many :cards, Hippo.Cards.Card
 
@@ -17,7 +19,8 @@ defmodule Hippo.Lanes.Lane do
   @doc false
   def changeset(lane, attrs) do
     lane
-    |> cast(attrs, [:title, :description])
+    |> cast(attrs, [:title, :description, :position])
     |> validate_required([:title])
+    |> set_rank(scope: :project_id)
   end
 end
