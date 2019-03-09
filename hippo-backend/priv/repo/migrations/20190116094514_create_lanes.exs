4667cfa3db109449e57d7cb4c3967364a9cecb51
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-03-09 14:07:23 +0100

    Add default values to rank column

--- hippo-backend/priv/repo/migrations/20190116094514_create_lanes.exs
+++ hippo-backend/priv/repo/migrations/20190116094514_create_lanes.exs
@@ -4,7 +4,7 @@ defmodule Hippo.Repo.Migrations.CreateLanes do
   def change do
     create table(:lanes, primary_key: false) do
       add :id, :binary_id, null: false, primary_key: true
-      add :rank, :integer
+      add :rank, :integer, null: false, default: 0
       add :title, :string
       add :description, :string, size: 500
       add :project_id, references(:projects, on_delete: :nothing, type: :binary_id)
--- hippo-backend/priv/repo/migrations/20190116123920_create_cards.exs
+++ hippo-backend/priv/repo/migrations/20190116123920_create_cards.exs
@@ -4,7 +4,7 @@ defmodule Hippo.Repo.Migrations.CreateCards do
   def change do
     create table(:cards, primary_key: false) do
       add :id, :binary_id, null: false, primary_key: true
-      add :rank, :integer
+      add :rank, :integer, null: false, default: 0
       add :title, :string
       add :description, :string, size: 500
       add :lane_id, references(:lanes, on_delete: :nothing, type: :binary_id)
