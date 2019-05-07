5316847c9f207b5976dbd8f6e0a6f9aea335585f
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-05-07 22:57:22 +0200

    Visually improved projects picker

--- hippo-frontend/src/components/ProjectPicker.js
+++ hippo-frontend/src/components/ProjectPicker.js
@@ -1,5 +1,13 @@
 import React, { useCallback } from "react";
-import { SideSheet, Position, Pane, Heading, Spinner } from "evergreen-ui";
+import {
+  Card,
+  SideSheet,
+  Position,
+  Pane,
+  Paragraph,
+  Heading,
+  Spinner
+} from "evergreen-ui";
 import { Query } from "react-apollo";
 import { gql } from "apollo-boost";
 import { either, pipe, prop, isNil, isEmpty } from "ramda";
@@ -22,35 +30,51 @@ const shouldRenderEmptyView = pipe(
 const ProjectPicker = props => {
   const closeHandler = useCallback(() => {
     props.history.goBack();
-  }, [props.history]);
+  }, [props]);
 
-  const HandleQuery = useCallback(({ loading, error, data }) => {
-    if (loading) return <Spinner />;
-    if (error) return <div>Uh, oh :(</div>;
-    if (shouldRenderEmptyView(data)) {
-      return <div>Nothing to show here..</div>;
-    }
+  const HandleQuery = useCallback(
+    ({ loading, error, data }) => {
+      if (loading) return <Spinner />;
+      if (error) return <div>Uh, oh :(</div>;
+      if (shouldRenderEmptyView(data)) {
+        return <div>Nothing to show here..</div>;
+      }
 
-    return (
-      <ul>
-        {data.projects.map(p => (
-          <li key={"p-" + p.id}>
-            <Link to={"projects/" + p.id}>{p.title}</Link>
-          </li>
-        ))}
-      </ul>
-    );
-  }, []);
+      return data.projects.map(p => (
+        <Card
+          key={"pp-" + p.id}
+          backgroundColor="white"
+          elevation={0}
+          height={240}
+          padding={20}
+          cursor="pointer"
+          onClick={() => props.history.push("projects/" + p.id)}
+        >
+          <Heading size={500}>{p.title}</Heading>
+          <Paragraph>{p.description}</Paragraph>
+        </Card>
+      ));
+    },
+    [props.history]
+  );
 
   return (
     <SideSheet
-      position={Position.LEFT}
       isShown={true}
       onCloseComplete={closeHandler}
+      position={Position.LEFT}
+      containerProps={{
+        display: "flex",
+        flex: "1",
+        flexDirection: "column"
+      }}
     >
-      <Pane padding="20px">
-        <Heading size={600}>Switch to Project</Heading>
-
+      <Pane zIndex={1} flexShrink={0} elevation={0} backgroundColor="white">
+        <Pane padding={16}>
+          <Heading size={600}>Switch to Project</Heading>
+        </Pane>
+      </Pane>
+      <Pane flex="1" overflowY="scroll" background="tint1" padding={16}>
         <Query query={GET_PROJECTS}>{HandleQuery}</Query>
       </Pane>
     </SideSheet>
