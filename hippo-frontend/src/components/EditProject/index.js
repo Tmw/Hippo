9ded67777672b6713efce8498347e06fefd24643
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-05-11 23:59:41 +0200

    Add edit project form

--- hippo-frontend/src/components/EditProject/index.js
+++ hippo-frontend/src/components/EditProject/index.js
@@ -1,5 +1,14 @@
 import React, { useCallback } from "react";
-import { SideSheet, Heading, Position, Pane } from "evergreen-ui";
+import {
+  Button,
+  SideSheet,
+  Heading,
+  TextInputField,
+  Textarea,
+  Label,
+  Position,
+  Pane
+} from "evergreen-ui";
 
 const EditProject = props => {
   const closeHandler = useCallback(() => {
@@ -11,10 +20,34 @@ const EditProject = props => {
       isShown={true}
       onCloseComplete={closeHandler}
       position={Position.LEFT}
+      containerProps={{
+        display: "flex",
+        flex: 1,
+        flexDirection: "column"
+      }}
     >
-      <Pane padding={36}>
-        <Heading size={600}>Edit Project</Heading>
-        <p>Another piece of text goes here</p>
+      <Pane zIndex={1} flexShrink={0} elevation={0} backgroundColor="white">
+        <Pane padding={16}>
+          <Heading size={600}>Edit Project</Heading>
+        </Pane>
+      </Pane>
+      <Pane flex="1" flexGrow={1} background="tint1" padding={16}>
+        <form>
+          <TextInputField
+            label="Project Title"
+            placeholder="Project Title"
+            required
+          />
+          <Label htmlFor="description" marginBottom={4} display="block">
+            Project Description
+          </Label>
+          <Textarea name="description" placeholder="Project Description" />
+
+          <Pane align="right" marginTop={16}>
+            <Button marginRight={12}>Cancel</Button>
+            <Button appearance="primary">Save</Button>
+          </Pane>
+        </form>
       </Pane>
     </SideSheet>
   );
