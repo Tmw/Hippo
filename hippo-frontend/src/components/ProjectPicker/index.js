5798daae612202be4d4603da35f82998c7a02131
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-05-15 23:27:25 +0200

    Optionally render SideSheet actions

--- hippo-frontend/src/components/ProjectPicker/index.js
+++ hippo-frontend/src/components/ProjectPicker/index.js
@@ -6,10 +6,20 @@ import { either, pipe, prop, isNil, isEmpty } from "ramda";
 import SidePanel from "components/SidePanel";
 import ProjectCard from "components/ProjectPicker/ProjectCard";
 import GET_PROJECTS from "graphql/get_projects_query";
+import { Tooltip, IconButton } from "evergreen-ui";
+import { Link } from "react-router-dom";
 
 const shouldRenderEmptyView = pipe(
   prop("projects"),
   either(isEmpty, isNil)
+);
+
+const SideSheetActionNewProject = () => (
+  <Tooltip content="Start new Project">
+    <Link to="/projects/new">
+      <IconButton appearance="minimal" icon="plus" />
+    </Link>
+  </Tooltip>
 );
 
 const ProjectPicker = props => {
@@ -37,7 +47,11 @@ const ProjectPicker = props => {
   );
 
   return (
-    <SidePanel onClose={closeHandler} title="Switch to Project">
+    <SidePanel
+      onClose={closeHandler}
+      title="Switch to Project"
+      panelActions={[<SideSheetActionNewProject />]}
+    >
       <Query query={GET_PROJECTS}>{HandleQuery}</Query>
     </SidePanel>
   );
--- hippo-frontend/src/components/SidePanel.js
+++ hippo-frontend/src/components/SidePanel.js
@@ -1,15 +1,7 @@
 import React from "react";
-import {
-  IconButton,
-  Tooltip,
-  SideSheet,
-  Pane,
-  Heading,
-  Position
-} from "evergreen-ui";
-import { Link } from "react-router-dom";
+import { SideSheet, Pane, Heading, Position } from "evergreen-ui";
 
-const SidePanel = ({ children, title, onClose }) => (
+const SidePanel = ({ children, title, onClose, panelActions }) => (
   <SideSheet
     isShown={true}
     onCloseComplete={onClose}
@@ -25,13 +17,7 @@ const SidePanel = ({ children, title, onClose }) => (
         <Heading size={600} flex={1}>
           {title}
         </Heading>
-        <Pane>
-          <Tooltip content="Start new Project">
-            <Link to="/projects/new">
-              <IconButton appearance="minimal" icon="plus" />
-            </Link>
-          </Tooltip>
-        </Pane>
+        <Pane>{panelActions}</Pane>
       </Pane>
     </Pane>
     <Pane flex="1" flexGrow={1} background="tint1" padding={16}>
