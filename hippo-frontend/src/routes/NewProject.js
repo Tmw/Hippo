dde748c3ee0ad2ca38f6e455938e2a0cc4b0b194
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-05-24 00:33:37 +0200

    Add Formik and implement CreateProjectMutation

--- hippo-frontend/src/routes/NewProject.js
+++ hippo-frontend/src/routes/NewProject.js
@@ -1,7 +1,15 @@
 import React, { useCallback } from "react";
+import { Pane, Button, TextInputField, Label, Textarea } from "evergreen-ui";
+import { Formik, Form } from "formik";
 import SidePanel from "components/SidePanel";
-import ProjectFormFields from "components/Forms/ProjectFormFields";
-import { Pane, Button } from "evergreen-ui";
+import FormikField from "components/FormikField";
+import CREATE_PROJECT_MUTATION from "graphql/create_project_mutation";
+import { Mutation } from "react-apollo";
+
+const initialValues = {
+  title: "",
+  description: ""
+};
 
 const NewProject = ({ history }) => {
   const closeHandler = useCallback(() => {
@@ -10,15 +18,47 @@ const NewProject = ({ history }) => {
 
   return (
     <SidePanel onClose={closeHandler} title="Start a new Project">
-      <form>
-        <ProjectFormFields />
-        <Pane display="flex" marginTop={16}>
-          <Pane flex={1} align="right">
-            <Button marginRight={12}>Cancel</Button>
-            <Button appearance="primary">Save</Button>
-          </Pane>
-        </Pane>
-      </form>
+      <Mutation mutation={CREATE_PROJECT_MUTATION}>
+        {createProject => (
+          <Formik
+            onSubmit={values => {
+              createProject({ variables: { project: values } });
+            }}
+            initialValues={initialValues}
+          >
+            <Form>
+              <FormikField
+                component={TextInputField}
+                name="title"
+                label="Project Title"
+                placeholder="Project Title"
+                tabIndex={0}
+              />
+
+              <Label htmlFor="description" marginBottom={4} display="block">
+                Project Description
+              </Label>
+
+              <FormikField
+                component={Textarea}
+                name="description"
+                placeholder="Project Description"
+              />
+
+              <Pane display="flex" marginTop={16}>
+                <Pane flex={1} align="right">
+                  <Button type="button" marginRight={12}>
+                    Cancel
+                  </Button>
+                  <Button type="submit" appearance="primary">
+                    Save
+                  </Button>
+                </Pane>
+              </Pane>
+            </Form>
+          </Formik>
+        )}
+      </Mutation>
     </SidePanel>
   );
 };
