48253470481b55d0a6ce72c7a6d36087d73cf1a5
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-06-08 17:29:02 +0200

    Introduce a empty state lane

--- hippo-frontend/src/components/Project.js
+++ hippo-frontend/src/components/Project.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { Pane } from "evergreen-ui";
+import { Button, Pane, Heading } from "evergreen-ui";
 import Lane from "components/Lane";
 import Header from "components/Header";
+
+const titleForAddLaneHeader = lanes => {
+  if (lanes.length > 0) {
+    return "Running out of space?";
+  } else {
+    return "Get started";
+  }
+};
 
 const Project = ({ project: { lanes }, project }) => (
   <React.Fragment>
@@ -16,6 +24,22 @@ const Project = ({ project: { lanes }, project }) => (
       {lanes.map(lane => (
         <Lane data={lane} key={lane.id} />
       ))}
+      <Pane
+        minWidth={350}
+        height="100%"
+        marginRight={15}
+        padding={25}
+        background="#f6f6f6"
+        className="Lane"
+        borderRadius="5px"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Heading>{titleForAddLaneHeader(lanes)}</Heading>
+        <Button iconBefore="plus">Add lane</Button>
+      </Pane>
     </Pane>
   </React.Fragment>
 );
