6dfd08a7a9fd34757cdfcc7d8e4210e00da803de
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-06-12 23:47:25 +0200

    Add menu for deleting and renaming to Lane

--- hippo-frontend/src/components/Lane/Header.js
+++ hippo-frontend/src/components/Lane/Header.js
@@ -1,5 +1,28 @@
 import React from "react";
-import { IconButton, Pane, Heading } from "evergreen-ui";
+import {
+  IconButton,
+  Pane,
+  Heading,
+  Popover,
+  Menu,
+  Position
+} from "evergreen-ui";
+
+const EditLaneMenu = () => (
+  <Menu>
+    <Menu.Group>
+      <Menu.Item icon="edit" secondaryText="⌘R">
+        Rename...
+      </Menu.Item>
+    </Menu.Group>
+    <Menu.Divider />
+    <Menu.Group>
+      <Menu.Item icon="trash" intent="danger">
+        Delete...
+      </Menu.Item>
+    </Menu.Group>
+  </Menu>
+);
 
 const Header = ({ title }) => (
   <Pane display="flex" marginRight={20} marginBottom={10} alignItems="center">
@@ -7,8 +30,12 @@ const Header = ({ title }) => (
       {title}
     </Heading>
 
-    <Pane align="right">
+    <Pane align="right" display="flex">
       <IconButton appearance="minimal" icon="plus" />
+
+      <Popover position={Position.BOTTOM_LEFT} content={EditLaneMenu}>
+        <IconButton appearance="minimal" icon="more" />
+      </Popover>
     </Pane>
   </Pane>
 );
