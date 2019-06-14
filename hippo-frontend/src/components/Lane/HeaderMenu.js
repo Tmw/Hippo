d94dfca67a8cced57fdd865c6700a53193d14d94
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-06-14 10:52:35 +0200

    Refactor lane popover menu

---
+++ hippo-frontend/src/components/Lane/HeaderMenu.js
@@ -0,0 +1,51 @@
+import React, { useCallback } from "react";
+import { compose } from "ramda";
+import { Menu, IconButton, Popover, Position } from "evergreen-ui";
+
+const HeaderMenu = ({ onDeleteClick, onEditClick }) => {
+  const popoverMenu = useCallback(
+    ({ close: closePopover }) => {
+      const handleEditClick = compose(
+        closePopover,
+        onEditClick
+      );
+
+      const handleDeleteClick = compose(
+        closePopover,
+        onDeleteClick
+      );
+
+      return (
+        <Menu>
+          <Menu.Group>
+            <Menu.Item icon="edit" onSelect={handleEditClick}>
+              Edit...
+            </Menu.Item>
+          </Menu.Group>
+          <Menu.Divider />
+          <Menu.Group>
+            <Menu.Item
+              icon="trash"
+              intent="danger"
+              onSelect={handleDeleteClick}
+            >
+              Delete...
+            </Menu.Item>
+          </Menu.Group>
+        </Menu>
+      );
+    },
+    [onDeleteClick, onEditClick]
+  );
+
+  return (
+    <React.Fragment>
+      <IconButton appearance="minimal" icon="plus" />
+
+      <Popover position={Position.BOTTOM_LEFT} content={popoverMenu}>
+        <IconButton appearance="minimal" icon="more" />
+      </Popover>
+    </React.Fragment>
+  );
+};
+export default HeaderMenu;
--- hippo-frontend/src/components/Lane/index.js
+++ hippo-frontend/src/components/Lane/index.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Pane } from "evergreen-ui";
 
 import Card from "components/Card";
 import Header from "components/Lane/Header";
 import Wrapper from "components/Lane/Wrapper";
+import HeaderMenu from "components/Lane/HeaderMenu";
 
-const Lane = ({ data: { title, cards } }) => (
-  <Wrapper>
-    <Header title={title} />
+const Lane = ({ data: { title, cards } }) => {
+  const handleDeleteLaneClicked = useCallback(() => {
+    console.log("handeling delete lane click");
+  }, []);
 
-    <Pane paddingRight="20px" overflowY="scroll">
-      {cards.map(c => (
-        <Card data={c} key={c.id} />
-      ))}
-    </Pane>
-  </Wrapper>
-);
+  const handleEditLaneClicked = useCallback(() => {
+    console.log("handeling edit lane click");
+  }, []);
+
+  return (
+    <Wrapper>
+      <Header title={title}>
+        <HeaderMenu
+          onDeleteClick={handleDeleteLaneClicked}
+          onEditClick={handleEditLaneClicked}
+        />
+      </Header>
+
+      <Pane paddingRight="20px" overflowY="scroll">
+        {cards.map(c => (
+          <Card data={c} key={c.id} />
+        ))}
+      </Pane>
+    </Wrapper>
+  );
+};
 
 export default Lane;
