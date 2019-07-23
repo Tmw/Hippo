3553de564502cb755cda5d057e37cd3935203117
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-07-23 21:48:43 +0200

    Introduce ConfirmAndMutate component that asks for confirmation before executing a mutation

---
+++ hippo-frontend/src/components/ConfirmAndMutate.js
@@ -0,0 +1,69 @@
+import React, { useState, useCallback } from "react";
+
+import { Dialog } from "evergreen-ui";
+
+const ConfirmAndMutate = ({
+  mutation,
+  title,
+  description,
+  isVisible,
+  closeDialog,
+  onSuccess,
+  onError
+}) => {
+  const [isInFlight, setInFlight] = useState(false);
+
+  const onCloseCompleteHandler = useCallback(() => closeDialog(), [
+    closeDialog
+  ]);
+
+  // perform the actual lane deletion mutation
+  const executeMutation = useCallback(() => {
+    setInFlight(true);
+
+    mutation()
+      .then(() => {
+        setInFlight(false);
+        closeDialog();
+        onSuccess();
+      })
+      .catch(error => {
+        setInFlight(false);
+        onError(error);
+        console.error(error);
+      });
+  }, [closeDialog, mutation, onError, onSuccess]);
+
+  return (
+    <Dialog
+      isShown={isVisible}
+      title={title}
+      intent="danger"
+      onCloseComplete={onCloseCompleteHandler}
+      onConfirm={executeMutation}
+      isConfirmLoading={isInFlight}
+      confirmLabel="Delete Card"
+    >
+      {description}
+    </Dialog>
+  );
+};
+
+const useConfirmAndMutationState = () => {
+  const initialState = { visible: false, cardId: null };
+  const [state, setState] = useState(initialState);
+
+  const showDialog = identifier =>
+    setState({ visible: true, identifier: identifier });
+
+  const closeDialog = () => setState(initialState);
+
+  return {
+    visible: state.visible,
+    identifier: state.identifier,
+    showDialog,
+    closeDialog
+  };
+};
+
+export { ConfirmAndMutate, useConfirmAndMutationState };
