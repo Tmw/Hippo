a6d1bf49e24dc84c381cbee3b3627dc7513ada00
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-07-24 22:34:58 +0200

    Implement dragging lanes, not persistent yet

--- hippo-frontend/src/components/Project.js
+++ hippo-frontend/src/components/Project.js
@@ -1,19 +1,11 @@
 import React, { useCallback } from "react";
-import { Pane } from "evergreen-ui";
 import { withRouter } from "react-router-dom";
 import { DragDropContext } from "react-beautiful-dnd";
-import CreateLaneCTA from "components/CreateLaneCTA";
 import LaneList from "components/LaneList";
 
 import Header from "components/Header";
 
 const Project = ({ project: { lanes }, project, history }) => {
-  // Lane creation callback
-  const handleAddClicked = useCallback(
-    () => history.push(`/projects/${project.id}/lanes/new`),
-    [history, project.id]
-  );
-
   const onDragEnd = useCallback(dragInfo => {
     console.log("dragInfo:", dragInfo);
   }, []);
@@ -21,13 +13,9 @@ const Project = ({ project: { lanes }, project, history }) => {
   return (
     <React.Fragment>
       <Header triggerTitle={project.title} />
-      <Pane width="100%" height="100%" display="flex" padding="25px">
-        <DragDropContext onDragEnd={onDragEnd}>
-          <LaneList lanes={lanes} projectId={project.id} />
-        </DragDropContext>
-
-        <CreateLaneCTA initial={lanes.length === 0} onAdd={handleAddClicked} />
-      </Pane>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <LaneList lanes={lanes} projectId={project.id} />
+      </DragDropContext>
     </React.Fragment>
   );
 };
