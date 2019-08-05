06dd8720b35290e823ac62c85d99cf0fdfc136b7
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-08-05 09:24:56 +0200

    Implement a realtime events subscription for project listing

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Hippo
+
 Lane based task management
+
+# Front-end
+
+## Dunning dev server
+
+```bash
+cd hippo-frontend && npm start
+```
+
+## Extracting updated Fragment / Union types
+
+In order for Apollo to resolve Fragments and Unions into their concrete types, it needs a little guidance from our back-end schema. Writing updates of the schema to disk can be done by running:
+
+```bash
+npm run get_fragment_types
+```
+
+The extracted fragment_types will be written to `src/graphql/fragmentTypes.json`
--- hippo-frontend/src/graphql/client.js
+++ hippo-frontend/src/graphql/client.js
@@ -1,15 +1,70 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from "apollo-client";
+import {
+  InMemoryCache,
+  IntrospectionFragmentMatcher
+} from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
 
-const uri = "http://localhost:4000/graphql";
+import * as AbsintheSocket from "@absinthe/socket";
+import { createAbsintheSocketLink } from "@absinthe/socket-apollo-link";
+import { Socket as PhoenixSocket } from "phoenix";
+import { split } from "apollo-link";
+import { getMainDefinition } from "apollo-utilities";
+
+import introspectionQueryResultData from "graphql/fragmentTypes.json";
+
+const httpUri = "http://localhost:4000/graphql";
+const wsUri = "ws://localhost:4000/socket";
+
+const httpLink = new HttpLink({ uri: httpUri });
+const wsLink = createAbsintheSocketLink(
+  AbsintheSocket.create(new PhoenixSocket(wsUri))
+);
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+  introspectionQueryResultData
+});
+
 export default new ApolloClient({
-  uri,
-  cacheRedirects: {
-    Query: {
-      lane: (_, { id }, { getCacheKey }) =>
-        getCacheKey({ __typename: "Lane", id }),
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    link
+  ]),
+  cache: new InMemoryCache({
+    fragmentMatcher,
+    cacheRedirects: {
+      Query: {
+        lane: (_, { id }, { getCacheKey }) =>
+          getCacheKey({ __typename: "Lane", id }),
 
-      card: (_, { id }, { getCacheKey }) =>
-        getCacheKey({ __typename: "Card", id })
+        card: (_, { id }, { getCacheKey }) =>
+          getCacheKey({ __typename: "Card", id })
+      }
     }
-  }
+  })
 });
