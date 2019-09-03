297e48735b15ecdc968a30542735b5f1aaf3623d
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2019-09-03 20:49:57 +0200

    Read all database config from ENV variables for CI

--- hippo-backend/config/test.exs
+++ hippo-backend/config/test.exs
@@ -11,8 +11,8 @@ config :logger, level: :warn
 
 # Configure your database
 config :hippo, Hippo.Repo,
-  username: "hippo",
-  password: "hippo",
-  database: "hippo-test",
+  username: System.get_env("DB_USER") || "hippo",
+  password: System.get_env("DB_PASS") || "hippo",
+  database: System.get_env("DB_NAME") || "hippo-test",
   hostname: System.get_env("DB_HOST") || "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
